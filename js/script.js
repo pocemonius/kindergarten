85345175c76e724c062938f6d2fb70a15e997365
pocemonius <ipocemonius@gmail.com>
2020-09-08 15:05:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,76 @@
+$(function(){
+
+    new WOW().init();
+
+    $(".main-slider").owlCarousel({
+        loop:true,
+        items:1,
+        nav:false,
+        animateOut:'fadeOut',
+        autoplay:true
+    });
+
+    $(".reviews-slider").owlCarousel({
+        loop:true,
+        items:1,
+        nav:false,
+        animateOut:'fadeOut',
+        autoplay:true
+    });
+
+    //кнопка прокрутки страницы вверх
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#scrollToTop').fadeIn();
+        } else {
+            $('#scrollToTop').fadeOut();
+        }
+    });
+    
+    $('#scrollToTop').click(function() {
+        $('body,html').animate({scrollTop:0},700);
+    });
+
+    //выпадающее меню
+    if( $(window).width() > 1201 ){
+        $(".header .menu>ul>li").hover(
+            function(){
+                $(this).children(".sub-menu").stop().slideDown();
+            }, 
+            function(){
+                $(this).children(".sub-menu").stop().hide();
+        });
+    }    
+
+    //прилипающее меню
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop(); 
+		if (scroll > 0) {
+			$(".header").addClass("fixed");
+		} else{
+			$(".header").removeClass("fixed");
+		}
+    });
+    
+    //открытие-закрытие адаптивного меню
+    $(".header .menu-toggle").on("click", function(){
+        var menu = $(this).next(".menu");
+        if( menu.hasClass("visible") ){
+            menu.removeClass("visible");
+            $(this).removeClass("active");            
+            $("html, body").css("overflow", "visible");
+        } else{
+            menu.addClass("visible");
+            $(this).addClass("active");
+            $("html, body").css("overflow", "hidden");
+            $(".header .menu>ul").animate({ scrollTop: 0 }, 500);
+        }
+    });
+
+    //открытие-закрытие подменю в адаптиве
+    $(".sub-menu-trigger").on("click", function(){
+        $(this).toggleClass("active");
+        $(this).next(".sub-menu").slideToggle();
+    });
+
+});
